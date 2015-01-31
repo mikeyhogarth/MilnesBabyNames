562c01704f75f2423af368334717613d91896e88
mikeyhogarth <mikehogarth20@hotmail.com>
2015-01-31 10:16:15 +0000

    better

--- app/helpers/guesses_helper.rb
+++ app/helpers/guesses_helper.rb
@@ -3,7 +3,10 @@ module GuessesHelper
   THE_NAMES = ["james", "alex", "alexander", "jim"]
 
   def got_it_right?(name)
-    THE_NAMES.include?(name.downcase) ? :correct : :incorrect
+    THE_NAMES.each do |actual_name|
+      return :correct if name.downcase.include? actual_name
+    end
+    :incorrect
   end
 
 end
